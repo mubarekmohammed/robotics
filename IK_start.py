0d1bea033aaa278a26c3339c33ad1e59b1c88fd6
Mubarek Mohammed <mubarekmohammed@Mubareks-MacBook-4.local>
2020-02-10 13:52:24 -0600

    first commit

--- IK_start.py
+++ IK_start.py
@@ -1,3 +1,6 @@
+#calculates the inverse kinematics of 3 DOF robot arm. The value of angles for joint 1 , 2 and 3 are calculated based 
+#on a set of points that correspond to a circle in a 3D plane
+
 import numpy as np 
 import matplotlib.pyplot as plt
 import csv
@@ -9,22 +12,62 @@ def main():
     y0_3 = 2
     x0_3 = 2
     z0_3 = 0
-    a_1 = 10
-    a_2 = 10
-    a_3 = 10
+    a_1 = 6
+    a_2 = 6
+    a_3 = 6
 
-    x = [2,4,6,4,3,4]
-    y = [2,5,7,5,3,2]
-    z = [0,2,3,5,1,4]
+    x = [2,
+    1.90211303259031,
+    1.61803398874990,
+    1.17557050458495,
+    0.618033988749895,
+    1.22464679914735e-16,
+    -0.618033988749895,
+    -1.17557050458495,
+    -1.61803398874989,
+    -1.90211303259031,
+    -2,
+    -1.90211303259031,
+    -1.61803398874990,
+    -1.17557050458495,
+    -0.618033988749895,
+    -3.67394039744206e-16,
+    0.618033988749895,
+    1.17557050458495,
+    1.61803398874989,
+    1.90211303259031,
+    2]
+    y = [0,
+    0.618033988749895,
+    1.17557050458495,
+    1.61803398874990,
+    1.90211303259031,
+    2,
+    1.90211303259031,
+    1.61803398874990,
+    1.17557050458495,
+    0.618033988749895,
+    2.44929359829471e-16,
+    -0.618033988749894,
+    -1.17557050458495,
+    -1.61803398874989,
+    -1.90211303259031,
+    -2,
+    -1.90211303259031,
+    -1.61803398874990,
+    -1.17557050458495,
+    -0.618033988749895,
+    -4.89858719658941e-16]
+    z = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-    theta_1a = [0 for i in range(6)]
-    theta_2a = [0 for i in range(6)]
-    theta_3a = [0 for i in range(6)]
+    theta_1a = [0 for i in range(21)]
+    theta_2a = [0 for i in range(21)]
+    theta_3a = [0 for i in range(21)]
 
     for i in range(len(x)):
         theta_1a[i] = np.arctan(y[i]/x[i])
-        theta_1a[i]*=(180/(np.pi))
-        r_1 = int(np.sqrt((x[i])^2 + (y[i])^2))
+        #theta_1a[i]*=(180/(np.pi))
+        r_1 = (np.sqrt((x[i])**2 + (y[i])**2))
         r_2 = z[i] - a_1
         phi_2 = np.arctan(r_2/r_1)
         #print(r_1)
@@ -34,14 +77,14 @@ def main():
         #print(r_1s)
         #print(r_2s)
         r_p = r_1s + r_2s
-        r_3 = int(np.sqrt(r_p))
+        r_3 = (np.sqrt(r_p))
         #print(r_3)
-        phi_1 = np.arccos(((a_3)^2 - (a_2)^2 - (r_3)^2)/(-2 * a_2 * r_3))
+        phi_1 = np.arccos(((a_3)**2 - (a_2)**2 - (r_3)**2)/(-2 * a_2 * r_3))
         theta_2a[i] = phi_2 - phi_1
-        theta_2a[i]*=(180/(np.pi))
-        phi_3 = np.arccos(((r_3)^2 - (a_2)^2 - (a_3)^2)/(-2 * a_2 * a_3))
+        #theta_2a[i]*=(180/(np.pi))
+        phi_3 = np.arccos(((r_3)**2 - (a_2)**2 - (a_3)**2)/(-2 * a_2 * a_3))
         theta_3a[i] = (np.pi * 2) - phi_3
-        theta_3a[i]*=(180/(np.pi))
+        #theta_3a[i]*=(180/(np.pi))
 
     theta_3a = np.array(theta_3a)
     theta_1a = np.array(theta_1a)
@@ -53,7 +96,7 @@ def main():
     np.savetxt('theta_1_values.csv', theta_1a, delimiter =',')
     np.savetxt('theta_2_values.csv', theta_2a, delimiter = ',')
     np.savetxt('theta_3_values.csv', theta_3a, delimiter = ',')
-    
+
 
 
     
